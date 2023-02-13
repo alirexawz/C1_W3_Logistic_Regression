ee2d857782f1d88e0c80962f605be52665778200
fragoa <alirexawz81@gmail.com>
2023-02-13 13:46:15 +0330

    Cost function for regularized logistic regression

--- C1_W3_Logistic_Regression.py
+++ C1_W3_Logistic_Regression.py
@@ -170,7 +170,29 @@ def predict(X, w, b):
             z_wb += b
 
             # Calculate the prediction from the model
-            f_wb = sigmoid(z_wb)
+            f_wb = sig(z_wb)
             p[i] = f_wb >= 0.5
           ### END CODE HERE ### 
           return p
+
+def compute_cost_reg(X, y, w, b, lambda_ = 1):
+
+         m, n = X.shape
+
+          # Calls the compute_cost function that you implemented above
+         cost_without_reg = compute_cost(X, y, w, b) 
+
+          # You need to calculate this value
+         reg_cost = 0.
+
+          ### START CODE HERE ###
+         for j in range(n):
+              reg_cost_j = w[j]**2
+              reg_cost = reg_cost + reg_cost_j
+
+          ### END CODE HERE ### 
+
+          # Add the regularization cost to get the total cost
+         total_cost = cost_without_reg + (lambda_/(2 * m)) * reg_cost
+
+         return total_cost
