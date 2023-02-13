296d8f5d8e8c63557f3a28063a180529c41b3cd8
fragoa <alirexawz81@gmail.com>
2023-02-13 13:44:35 +0330

    Evaluating logistic regression

--- C1_W3_Logistic_Regression.py
+++ C1_W3_Logistic_Regression.py
@@ -148,3 +148,29 @@ def compute_gradient(X, y, w, b, lambda_=None):
             print(f"Iteration {i:4}: Cost {float(J_history[-1]):8.2f}   ")
         
     return w_in, b_in, J_history, w_history #return w and J,w history for graphing
+
+
+def predict(X, w, b): 
+          # number of training examples
+          m, n = X.shape   
+          p = np.zeros(m)
+
+          ### START CODE HERE ### 
+          # Loop over each example
+          for i in range(m):   
+            # Calculate f_wb (exactly how you did it in the compute_cost function above)
+            z_wb = 0
+            # Loop over each feature
+            for j in range(n): 
+                     # Add the corresponding term to z_wb
+                     z_wb_ij = X[i, j] * w[j]
+                     z_wb += z_wb_ij
+
+            # Add bias term 
+            z_wb += b
+
+            # Calculate the prediction from the model
+            f_wb = sigmoid(z_wb)
+            p[i] = f_wb >= 0.5
+          ### END CODE HERE ### 
+          return p
